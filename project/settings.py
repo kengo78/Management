eefd3453a21efc63bd543986060131e224483894
kengo78 <fulken35@gmail.com>
2022-05-21 18:00:36 +0900

    modi

--- project/settings.py
+++ project/settings.py
@@ -6,11 +6,11 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
+# env = environ.Env()
+# env.read_env(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.get_value('DEBUG', cast = bool)
+# SECRET_KEY = env('SECRET_KEY')
+# DEBUG = env.get_value('DEBUG', cast = bool)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
